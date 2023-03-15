a72a34280a9b149fc0b88cd1626b6f1f3f440475
xplct-cont <kennsecusana@gmail.com>
2023-03-15 08:29:20 +0800

    Removed f-config

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import firebase from 'firebase'
 import { ref, onUnmounted } from 'vue'
 
 const config = {
-  apiKey: "AIzaSyDuIUxc8SP7QZs4J_y2Flnk_wytDx0GG-k",
-  authDomain: "vue-f-1f1bb.firebaseapp.com",
-  projectId: "vue-f-1f1bb",
-  storageBucket: "vue-f-1f1bb.appspot.com",
-  messagingSenderId: "629789211006",
-  appId: "1:629789211006:web:bc7fe42dbd315282b41b2f"
+   //Firebase Configuration Here!
 }
 
 const firebaseApp = firebase.initializeApp(config)
